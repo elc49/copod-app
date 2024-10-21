6345d0566fd3d8ce7b1894913cbd047422d16b30
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-10-21 22:58:15 +0300

    feat: telants can prove land usage rights

--- registry/hardhat.config.ts
+++ registry/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ignition-ethers"
 import "@nomicfoundation/hardhat-ethers"
 import "@nomicfoundation/hardhat-chai-matchers"
+import "@nomicfoundation/hardhat-network-helpers"
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
