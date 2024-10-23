f8ae6e971c770648396110a779619b60c456a3fa
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-10-23 18:48:13 +0300

    Android

--- Android/app/src/main/java/com/lomolo/copodapp/navigation/Root.kt
+++
@@ -1,6 +0,0 @@
-package com.lomolo.copodapp.navigation
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Navigation() {}
--- Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Login.kt
+++
@@ -1,9 +0,0 @@
-package com.lomolo.copodapp.ui.screens
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-
-@Composable
-fun LoginScreen(
-    modifier: Modifier,
-) {}
