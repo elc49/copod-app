8f2665687d00285dd78c8df23be3673d6e8ee3ba
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-10-26 11:47:49 +0300

    ui: error text style

--- Android/app/src/main/java/com/lomolo/copodv2/ui/screens/Web3SdkError.kt
+++ Android/app/src/main/java/com/lomolo/copodv2/ui/screens/Web3SdkError.kt
@@ -3,10 +3,12 @@ package com.lomolo.copodv2.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import com.lomolo.copodv2.ui.navigation.Navigation
 
 object Web3SdkErrorScreenDestination: Navigation {
@@ -23,6 +25,11 @@ fun Web3SdkErrorScreen(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Text("Something went wrong")
+        Text(
+            "Something went wrong",
+            style = MaterialTheme.typography.titleMedium,
+            color = MaterialTheme.colorScheme.error,
+            textAlign = TextAlign.Center,
+        )
     }
 }
