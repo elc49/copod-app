81d488de66104cea085611786eca8165e934d566
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-10-28 23:55:51 +0300

    graphql: get local lands

--- Android/app/src/main/java/com/lomolo/copodv2/network/Apollo.kt
+++
@@ -1,9 +0,0 @@
-package com.lomolo.copodv2.network
-
-import com.apollographql.apollo3.ApolloClient
-
-interface GraphQL {}
-
-class GraphQLServiceImpl(
-    private val apolloClient: ApolloClient
-): GraphQL {}
