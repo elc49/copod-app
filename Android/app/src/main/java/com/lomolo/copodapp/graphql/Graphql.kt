f83cb8ceba2a2337c8201deda408e71c7120feee
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-11-10 19:58:26 +0300

    chore(ws): setup websockets

--- Android/app/src/main/java/com/lomolo/copodapp/graphql/Graphql.kt
+++ Android/app/src/main/java/com/lomolo/copodapp/graphql/Graphql.kt
@@ -4,15 +4,24 @@ import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.cache.normalized.normalizedCache
 import com.apollographql.apollo.cache.normalized.sql.SqlNormalizedCacheFactory
 import com.apollographql.apollo.network.okHttpClient
+import com.apollographql.apollo.network.ws.GraphQLWsProtocol
 import com.lomolo.copodapp.BuildConfig
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 
 fun getGraphqlClient(httpClient: OkHttpClient): ApolloClient {
     val sqlCacheStore = SqlNormalizedCacheFactory("graphql.db")
     val baseApi = BuildConfig.apilocal
-    return ApolloClient.Builder()
-        .okHttpClient(okHttpClient = httpClient)
-        .httpServerUrl("${baseApi}/graphql")
-        .normalizedCache(sqlCacheStore)
-        .build()
+    val baseWssApi = BuildConfig.wssapilocal
+    return ApolloClient.Builder().okHttpClient(okHttpClient = httpClient)
+        .httpServerUrl("${baseApi}/graphql").webSocketServerUrl("${baseWssApi}/graphql")
+        .wsProtocol(GraphQLWsProtocol.Factory()).webSocketReopenWhen { _, attempt ->
+            CoroutineScope(Dispatchers.IO).launch {
+                delay(attempt * 1000)
+            }
+            true
+        }.normalizedCache(sqlCacheStore).build()
 }
