d67ef53ade6283cf20584457b631e12718279f58
edwinlomolo <25983711+elc49@users.noreply.github.com>
2024-12-19 12:35:38 +0300

    chore(ui): account dialog title

--- Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Account.kt
+++ Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Account.kt
@@ -3,6 +3,7 @@ package com.lomolo.copodapp.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -19,12 +20,12 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.lomolo.copodapp.R
-import com.web3auth.core.types.UserInfo
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -41,12 +42,20 @@ fun AccountDetails(
             tonalElevation = AlertDialogDefaults.TonalElevation,
         ) {
             Column {
-                IconButton(onClick = {
-                    setDialog(false)
-                }) {
-                    Icon(
-                        Icons.TwoTone.Close,
-                        contentDescription = stringResource(R.string.close),
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    IconButton(onClick = {
+                        setDialog(false)
+                    }) {
+                        Icon(
+                            Icons.TwoTone.Close,
+                            contentDescription = stringResource(R.string.close),
+                        )
+                    }
+                    Text(
+                        stringResource(R.string.account),
+                        style = MaterialTheme.typography.titleLarge,
                     )
                 }
                 Column(
