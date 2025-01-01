4bd0cb58d1788e0c047be892d585cbe277e8a4aa
edwinlomolo <25983711+elc49@users.noreply.github.com>
2025-01-01 17:08:37 +0300

    found land screen

--- Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Search.kt
+++ Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Search.kt
@@ -1,6 +1,7 @@
 package com.lomolo.copodapp.ui.screens
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +12,11 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.SearchBarDefaults
@@ -50,15 +51,16 @@ fun SearchLandScreen(
     currentDestination: NavDestination,
     onNavigateTo: (String) -> Unit,
 ) {
-    Scaffold(
-        topBar = {
-            SearchLandTopBar()
-        },
-        bottomBar = {
-            BottomNavBar(currentDestination = currentDestination, onNavigateTo = onNavigateTo)
-        }
-    ) { innerPadding ->
-        Surface(modifier = modifier.fillMaxSize().padding(innerPadding)) {  }
+    Scaffold(topBar = {
+        SearchLandTopBar()
+    }, bottomBar = {
+        BottomNavBar(currentDestination = currentDestination, onNavigateTo = onNavigateTo)
+    }) { innerPadding ->
+        Surface(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {}
     }
 }
 
@@ -101,18 +103,79 @@ fun SearchLandTopBar(
             onExpandedChange = { expanded = it },
             expanded = expanded,
         ) {
-            Column(Modifier.verticalScroll(rememberScrollState())) {
-                repeat(4) { idx ->
-                    val resultText = "Suggestion $idx"
-                    ListItem(headlineContent = { Text(resultText) },
-                        supportingContent = { Text("Additional info") },
-                        leadingContent = { Icon(Icons.Filled.Star, contentDescription = null) },
-                        modifier = Modifier
-                            .clickable {}
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp, vertical = 4.dp))
-                }
-            }
+            FoundLand()
+        }
+    }
+}
+
+@Composable
+private fun FoundLand(
+    modifier: Modifier = Modifier,
+) {
+    val users = 100
+
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        Text(
+            stringResource(R.string.users_with_usage_rights),
+            style = MaterialTheme.typography.titleLarge,
+        )
+        repeat(4) { idx ->
+            val resultText = "User $idx"
+            ListItem(headlineContent = {
+                Text(
+                    resultText,
+                    style = MaterialTheme.typography.titleSmall,
+                )
+            },
+                supportingContent = {
+                    Text(
+                        "email@email.com",
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.secondary
+                    )
+                },
+                modifier = Modifier
+                    .clickable {}
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp))
+        }
+        Text(
+            stringResource(R.string.land_registration_date),
+            style = MaterialTheme.typography.titleLarge,
+        )
+
+        Column {
+            Text(
+                "January 1, 2024",
+                style = MaterialTheme.typography.titleSmall,
+            )
+            Text(
+                stringResource(R.string.land_registered_date),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.secondary,
+            )
+        }
+
+        Text(
+            stringResource(R.string.previous_users),
+            style = MaterialTheme.typography.titleLarge,
+        )
+
+        Column {
+            Text(
+                stringResource(R.string.total, users),
+                style = MaterialTheme.typography.titleSmall,
+            )
+            Text(
+                stringResource(R.string.had_usage_rights, users),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.secondary,
+            )
         }
     }
 }
