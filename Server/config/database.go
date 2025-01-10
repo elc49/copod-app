78095a1284397fe7361d86781e19e490425cd87c
edwinlomolo <25983711+elc49@users.noreply.github.com>
2025-01-10 13:00:14 +0300

    chore: setup deployments

--- Server/config/database.go
+++ Server/config/database.go
@@ -2,10 +2,9 @@ package config
 
 import (
 	"github.com/elc49/copod/config/postgres"
-	"github.com/elc49/copod/config/redis"
 )
 
 type Database struct {
 	Rdbms postgres.Postgres
-	Redis redis.Redis
+	Redis string
 }
