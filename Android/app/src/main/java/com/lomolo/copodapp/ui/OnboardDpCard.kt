f85575f1f2a031d2e1e2b5f4dec82fdb16480095
edwinlomolo <25983711+elc49@users.noreply.github.com>
2025-01-21 09:05:38 +0300

    greatly improve onboarding flow

---
+++ Android/app/src/main/java/com/lomolo/copodapp/ui/OnboardDpCard.kt
@@ -0,0 +1,76 @@
+package com.lomolo.copodapp.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedIconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.lomolo.copodapp.R
+
+@Composable
+fun OnboardDpCard(
+    modifier: Modifier = Modifier,
+    onNavigateToUploadDp: () -> Unit,
+    land: String,
+    govtId: String,
+) {
+    OutlinedCard(
+        modifier
+            .fillMaxWidth()
+            .height(360.dp)
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .padding(12.dp)
+        ) {
+            Column(
+                Modifier
+                    .align(Alignment.TopStart),
+            ) {
+                Icon(
+                    painterResource(R.drawable.account),
+                    modifier = Modifier.size(32.dp),
+                    contentDescription = stringResource(R.string.display_picture),
+                )
+                Spacer(Modifier.size(20.dp))
+                Text(
+                    stringResource(R.string.display_picture),
+                    style = MaterialTheme.typography.displaySmall,
+                )
+                Text(
+                    stringResource(R.string.picture_of_you)
+                )
+            }
+            Text(
+                stringResource(R.string.upload_dp_copy_text),
+                modifier = Modifier.align(Alignment.CenterStart),
+            )
+            OutlinedIconButton(
+                onClick = onNavigateToUploadDp,
+                modifier = Modifier.align(Alignment.BottomEnd).size(60.dp),
+                enabled = land.isNotEmpty() && govtId.isNotEmpty(),
+            ) {
+                Icon(
+                    painterResource(R.drawable.doc_paper),
+                    modifier = Modifier.size(32.dp),
+                    contentDescription = stringResource(R.string.upload)
+                )
+            }
+        }
+    }
+}
--- Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Onboarding.kt
+++ Android/app/src/main/java/com/lomolo/copodapp/ui/screens/Onboarding.kt
@@ -1,36 +1,29 @@
 package com.lomolo.copodapp.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.twotone.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedCard
-import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.lomolo.copodapp.R
 import com.lomolo.copodapp.state.viewmodels.OnboardingViewModel
+import com.lomolo.copodapp.ui.OnboardDpCard
+import com.lomolo.copodapp.ui.OnboardGovtIdCard
+import com.lomolo.copodapp.ui.OnboardLandCard
 import com.lomolo.copodapp.ui.common.TopBar
 import com.lomolo.copodapp.ui.navigation.Navigation
 
@@ -81,141 +74,15 @@ fun OnboardingScreen(
                     ),
                 verticalArrangement = Arrangement.spacedBy(12.dp),
             ) {
-                OutlinedCard(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(360.dp)
-                ) {
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .padding(12.dp)
-                    ) {
-                        Column(
-                            Modifier
-                                .align(Alignment.TopStart),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.earth),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.earth),
-                            )
-                            Spacer(Modifier.size(20.dp))
-                            Text(
-                                stringResource(R.string.land_title),
-                                style = MaterialTheme.typography.displaySmall,
-                            )
-                            Text(
-                                stringResource(R.string.upload_govt_issued_title)
-                            )
-                        }
-                        Text(
-                            stringResource(R.string.land_uplod_copy_text),
-                            modifier = Modifier.align(Alignment.CenterStart),
-                        )
-                        OutlinedIconButton(
-                            onClick = onNavigateToUploadLandTitle,
-                            modifier = Modifier.align(Alignment.BottomEnd).size(60.dp),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.doc_paper),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.upload)
-                            )
-                        }
-                    }
-                }
-                OutlinedCard(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(360.dp),
-                ) {
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .padding(12.dp)
-                    ) {
-                        Column(
-                            Modifier
-                                .align(Alignment.TopStart),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.govt_id),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.upload_govt_issued_id),
-                            )
-                            Spacer(Modifier.size(20.dp))
-                            Text(
-                                stringResource(R.string.govt_issued_id),
-                                style = MaterialTheme.typography.displaySmall,
-                            )
-                            Text(
-                                stringResource(R.string.upload_govt_issued_id)
-                            )
-                        }
-                        Text(
-                            stringResource(R.string.verify_your_id_copy_text),
-                            modifier = Modifier.align(Alignment.CenterStart),
-                        )
-                        OutlinedIconButton(
-                            onClick = onNavigateToUploadGovtId,
-                            modifier = Modifier.align(Alignment.BottomEnd).size(60.dp),
-                            enabled = land.isNotEmpty(),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.doc_paper),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.upload)
-                            )
-                        }
-                    }
-                }
-                OutlinedCard(
-                    Modifier
-                        .fillMaxWidth()
-                        .height(360.dp)
-                ) {
-                    Box(
-                        Modifier
-                            .fillMaxSize()
-                            .padding(12.dp)
-                    ) {
-                        Column(
-                            Modifier
-                                .align(Alignment.TopStart),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.account),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.display_picture),
-                            )
-                            Spacer(Modifier.size(20.dp))
-                            Text(
-                                stringResource(R.string.display_picture),
-                                style = MaterialTheme.typography.displaySmall,
-                            )
-                            Text(
-                                stringResource(R.string.picture_of_you)
-                            )
-                        }
-                        Text(
-                            stringResource(R.string.upload_dp_copy_text),
-                            modifier = Modifier.align(Alignment.CenterStart),
-                        )
-                        OutlinedIconButton(
-                            onClick = onNavigateToUploadDp,
-                            modifier = Modifier.align(Alignment.BottomEnd).size(60.dp),
-                            enabled = land.isNotEmpty() && govtId.isNotEmpty(),
-                        ) {
-                            Icon(
-                                painterResource(R.drawable.doc_paper),
-                                modifier = Modifier.size(32.dp),
-                                contentDescription = stringResource(R.string.upload)
-                            )
-                        }
-                    }
-                }
+                OnboardLandCard(onNavigateToUploadLandTitle = onNavigateToUploadLandTitle)
+                OnboardGovtIdCard(onNavigateToUploadGovtId = onNavigateToUploadGovtId, land = land)
+                OnboardDpCard(onNavigateToUploadDp = onNavigateToUploadDp, land = land, govtId = govtId)
             }
         }
     }
 }
+
+
+
+
+
