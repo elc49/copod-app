2cb3938f39390cdb663d793d1d4a9ee4fc9dd45f
edwinlomolo <25983711+elc49@users.noreply.github.com>
2025-01-23 23:20:25 +0300

    fix: land divisibility constant

--- Server/util/blockchain.go
+++ Server/util/blockchain.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"math/big"
 	"reflect"
 	"regexp"
 
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/shopspring/decimal"
 )
 
 // IsValidAddress validate eth address
@@ -39,4 +41,46 @@ func IsZeroAddress(a interface{}) bool {
 	zeroAddressBytes := common.FromHex("0x0000000000000000000000000000000000000000")
 	addressBytes := address.Bytes()
 	return reflect.DeepEqual(zeroAddressBytes, addressBytes)
+}
+
+// ToWei decimal to wei
+func ToWei(iamount interface{}, decimals int) *big.Int {
+	amount := decimal.NewFromFloat(0)
+	switch v := iamount.(type) {
+	case string:
+		amount, _ = decimal.NewFromString(v)
+	case float64:
+		amount = decimal.NewFromFloat(v)
+	case int64:
+		amount = decimal.NewFromInt(v)
+	case decimal.Decimal:
+		amount = v
+	case *decimal.Decimal:
+		amount = *v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	result := amount.Mul(mul)
+
+	wei := new(big.Int)
+	wei.SetString(result.String(), 10)
+
+	return wei
+}
+
+// ToDecimal wei to decimals
+func ToDecimals(iamount interface{}, decimals int) decimal.Decimal {
+	value := new(big.Int)
+	switch v := iamount.(type) {
+	case string:
+		value.SetString(v, 10)
+	case *big.Int:
+		value = v
+	}
+
+	mul := decimal.NewFromFloat(float64(10)).Pow(decimal.NewFromFloat(float64(decimals)))
+	num, _ := decimal.NewFromString(value.String())
+	result := num.Div(mul)
+
+	return result
 }
