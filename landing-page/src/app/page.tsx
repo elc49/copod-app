f9ad14b785f107cf8a8d7d642c2aa936b382fc71
edwinlomolo <25983711+elc49@users.noreply.github.com>
2025-01-28 16:50:32 +0300

    chore(landing): marketing

--- landing-page/src/app/page.tsx
+++ landing-page/src/app/page.tsx
@@ -9,11 +9,11 @@ export default function Home() {
     <Box bgGradient="to-b" gradientFrom="gray.900" gradientTo="gray.800" h="dvh" color="white">
       <Container mx="auto" px="4" pt="20" pb="32">
         <Box maxW="4xl" mx="auto" textAlign="center">
-          <Heading size="5xl" mb="6" fontWeight="bold">
-            The Future of Land Ownership is Digital
+          <Heading size={{ base: "5xl", md: "6xl", lg: "7xl" }} mb="6" fontWeight="bold">
+            Find. Request. Acquire Land Usage Rights
           </Heading>
           <Text fontSize="xl" color="gray.300" mb="12">
-            Join the first decentralized land ownership platform on Ethereum. Secure, transparent, and borderless property transactions
+            Join the first real-world land ownership platform on Ethereum blockchain. Secure, Transparent, and Borderless land use agreements
           </Text>
           <Box maxW="md" mx="auto">
             <EmailForm />
@@ -28,10 +28,10 @@ export default function Home() {
                 <Lock boxSize="48px" color="blue.500" />
               </Flex>
               <Heading size="xl" fontWeight="bold" mb="2">
-                Secure Transactions
+                Secure & Trustless Agreements
               </Heading>
               <Text color="gray.400">
-                Smart contracts ensure safe and transparent property transactions
+                Smart contracts ensure safe and transparent land usage agreements
               </Text>
             </Box>
             <Box textAlign="center">
@@ -39,10 +39,10 @@ export default function Home() {
                 <Landmark boxSize="48px" color="blue.500" />
               </Flex>
               <Heading size="xl" fontWeight="bold" mb="2">
-                Verified properties
+                Verified land and land owners
               </Heading>
               <Text color="gray.400">
-                All listings are verified and authenticated on the blockchain
+                Government issued titles are zkp-verified and on the blockchain
               </Text>
             </Box>
             <Box textAlign="center">
@@ -50,10 +50,10 @@ export default function Home() {
                 <GlobeEarth boxSize="48px" color="blue.500" />
               </Flex>
               <Heading size="xl" fontWeight="bold" mb="2">
-                Global access
+                Arable. Semi-arid. Arid
               </Heading>
               <Text color="gray.400">
-                Buy property rights from anywhere in the world
+                Farming, Camping. Find your use case
               </Text>
             </Box>
           </SimpleGrid>
